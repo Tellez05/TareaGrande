36e7246f3211152a4fb47e25e2b040bcc531faf1
Tellez05 <A01640907@tec.mx>
2025-09-25 13:35:11 -0600

    Ya esta nada mas tengo la duda de los archivos

--- Functions.cpp
+++ Functions.cpp
@@ -97,9 +97,9 @@ void ArreglarArreglo(vector<Documento*> &Libreria, vector<int> &IndicesMeses){
     int JUN, FJUN, JUL, FJUL, AUG, FAUG, SEP, FSEP,OCT, FOCT; 
     JUN = 0;
     FJUN = MesJUN.size()-1; 
-    JUL = FJUL + 1;
+    JUL = FJUN + 1;
     FJUL = JUL+MesJUL.size();
-    AUG = FAUG + 1; 
+    AUG = FJUL + 1; 
     FAUG = AUG + MesAUG.size(); 
     SEP = FAUG + 1; 
     FSEP = SEP + MesSEP.size(); 
@@ -126,18 +126,127 @@ void EntregarDocumento(vector<Documento*> Libreria){
     Myfile.close();
 }
 
-void SacarBusq(vector<Documento*> Libreria, int contadorInicio, int contadorFinal){ 
-
+void SacarDiaMes(string &PrimerMes, string &SegundoMes, int &PrimerDia, int &SegundoDia, string PrimeraBusqueda, string SegundaBusqueda){
+    stringstream ss(PrimeraBusqueda);
+    ss >> PrimerMes >> PrimerDia;
+    stringstream ss2(SegundaBusqueda);
+    ss2 >> SegundoMes >> SegundoDia;  
 }
 
-void EntregarDocumentoBusq(vector<Documento*> Libreria, vector<int> IndiceMeses){
+void SacarPrimerContador (int &contadorInicial ,string PrimerMes, int PrimerDia, vector<int> IndiceMeses, vector<Documento*> Libreria){
+    int contadorMes{0};
+    int finalMes{0};
+    if(PrimerMes == "Jun"){
+        contadorMes = IndiceMeses[0];
+        finalMes = IndiceMeses[1];
+    }
+    else if(PrimerMes == "Jul"){
+        contadorMes = IndiceMeses[2];
+        finalMes = IndiceMeses[3]; 
+    }
+    else if(PrimerMes == "Aug"){
+        contadorMes = IndiceMeses[4];
+        finalMes = IndiceMeses[5];
 
+    }
+    else if(PrimerMes == "Sep"){
+        contadorMes = IndiceMeses[6];
+        finalMes = IndiceMeses[7];
+    }else{
+        contadorMes = IndiceMeses[8];
+        finalMes = IndiceMeses[9];
+    }
+    
+    int Inicio {contadorMes};
+    int Medio {(Inicio + finalMes)/2}; 
+    while(Inicio <= finalMes){
+        Medio = (Inicio + finalMes)/2;
+        if(PrimerDia == Libreria[Medio] -> regresarDia()){
+            contadorInicial = Medio; 
+            break; 
+        }
+        else if(PrimerDia < Libreria[Medio] -> regresarDia()){
+            finalMes = Medio -1; 
+        }
+        else{
+            Inicio = Medio +1; 
+        }
+    }
+
+    while(Libreria[contadorInicial] -> regresarDia() == PrimerDia && Libreria[contadorInicial-1] -> regresarDia() == PrimerDia && contadorInicial > contadorMes){
+        contadorInicial -= 1; 
+    }
+
+    
+}
+
+void SacarSegundoContador (int &contadorFinal ,string SegundoMes, int SegundoDia, vector<int> IndiceMeses, vector<Documento*> Libreria){
+    int contadorMes{0};
+    int finalMes{0};
+    if(SegundoMes == "Jun"){
+        contadorMes = IndiceMeses[0];
+        finalMes = IndiceMeses[1];
+    }
+    else if(SegundoMes == "Jul"){
+        contadorMes = IndiceMeses[2];
+        finalMes = IndiceMeses[3]; 
+    }
+    else if(SegundoMes == "Aug"){
+        contadorMes = IndiceMeses[4];
+        finalMes = IndiceMeses[5];
+
+    }
+    else if(SegundoMes == "Sep"){
+        contadorMes = IndiceMeses[6];
+        finalMes = IndiceMeses[7];
+    }else{
+        contadorMes = IndiceMeses[8];
+        finalMes = IndiceMeses[9];
+    }
+    
+    int Inicio {contadorMes};
+    int Medio {(Inicio + finalMes)/2}; 
+    while(Inicio <= finalMes){
+        Medio = (Inicio + finalMes)/2;
+        if(SegundoDia == Libreria[Medio] -> regresarDia()){
+            contadorFinal = Medio; 
+            break; 
+        }
+        else if(SegundoDia < Libreria[Medio] -> regresarDia()){
+            finalMes = Medio -1; 
+        }
+        else{
+            Inicio = Medio +1; 
+        }
+    }
+
+    while(Libreria[contadorFinal] -> regresarDia() == SegundoDia && Libreria[contadorFinal+1] -> regresarDia() == SegundoDia && contadorFinal < finalMes){
+        contadorFinal += 1; 
+    }
+
+    
+}
+
+void SacarBusq(vector<Documento*> Libreria, vector<int> IndicesMeses, string BusquedaInicial, string BusquedaFinal,  int &contadorInicio, int &contadorFinal){ 
+    string PrimerMes, SegundoMes; 
+    int PrimerDia, SegundoDia;
+    SacarDiaMes(PrimerMes, SegundoMes, PrimerDia, SegundoDia, BusquedaInicial, BusquedaFinal);
+    SacarPrimerContador(contadorInicio, PrimerMes, PrimerDia, IndicesMeses, Libreria);
+    SacarSegundoContador(contadorFinal, SegundoMes, SegundoDia, IndicesMeses, Libreria);
+}
+
+void EntregarDocumentoBusq(vector<Documento*> Libreria, vector<int> IndiceMeses, int Inicio, int Final, string nombre){
+    ofstream MyDocument{nombre};
+    for(int i {Inicio}; i <= Final; i++){
+        MyDocument << Libreria[i] -> RegresarLinea()<<endl;
+    }
+    MyDocument.close();
 }
 string CreadorNombre(int contador){
     string numero {to_string(contador)};
     return "Salida"+numero+"-eq5"; 
 }
-bool Menu(vector<Documento*> Libreria, vector<int> IndiceMeses){
+bool Menu(vector<Documento*> Libreria, vector<int> IndiceMeses, int &contador){
     cout<<"Menu: "<<endl; 
     cout<<"1.- Busqueda"<<endl; 
     cout<<"2.- Salir"<<endl; 
@@ -145,7 +254,19 @@ bool Menu(vector<Documento*> Libreria, vector<int> IndiceMeses){
     int opcion {0}; 
     cin >> opcion ; 
     if(opcion == 1){
-        EntregarDocumentoBusq(Libreria, IndiceMeses); 
+        int Inicio, Final; 
+        cout<<"Formato es: Mes Dia,  (Eje: Oct 8) "<<endl; 
+        string busquedaInicial, busquedaFinal, Nombre;
+        Nombre = CreadorNombre(contador);
+        cout<<"Que Mes y dia quieres buscar: ";
+        cin.ignore(); 
+        getline(cin,busquedaInicial); 
+        cout<<endl;
+        cout<<"Hasta que Mes y dia quieres buscar: ";
+        getline(cin,busquedaFinal);
+        SacarBusq(Libreria, IndiceMeses, busquedaInicial, busquedaFinal, Inicio, Final); 
+        EntregarDocumentoBusq(Libreria, IndiceMeses, Inicio, Final, Nombre); 
+        contador += 1; 
         return true; 
     }else if(opcion == 2){
         return false; 
--- main.cpp
+++ main.cpp
@@ -3,13 +3,14 @@
 
 int main(){
     vector<Documento*> Libreria;
+    int Contador {1};
     vector<int> IndicesMeses;  
     CargarLibreria(Libreria); 
     ArreglarArreglo(Libreria, IndicesMeses); 
     EntregarDocumento(Libreria);
     bool V {true}; 
     while(V == true){
-        V = Menu(Libreria,IndicesMeses); 
+        V = Menu(Libreria,IndicesMeses, Contador); 
     }
     return 0; 
 }
